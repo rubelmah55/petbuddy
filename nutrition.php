b60a5548e51a326c1e482b9a970e3f5ce36c5020
RajuAhmed2 <92273686+RajuAhmed2@users.noreply.github.com>
2022-05-14 10:44:57 +0600

    sum change & preloder change

--- nutrition.php
+++ nutrition.php
@@ -1,12 +1,13 @@
 <?php
 /*
-Template Name: Preloder Page
+Template Name: nutrition Plan
 
 */
 get_header(); 
 ?>
+
 <!-- header-2 -->
-    <header class="head-wrapper">
+<header class="head-wrapper">
         <div class="container">
             <div class="head-top">
                 <div class="row  align-items-center">
@@ -66,20 +67,205 @@ get_header();
         </div>
     </header>
 <!-- header-2 -->
-
-<!-- preloder -->
-    <div class="preloder-wepeer">
+<!-- nutrition Plan -->
+<!-- section-1 -->
+    <div class="nutrition-wepper">
+        <div class="container">
+            <div class="header text-center">
+               <h4>Хранителен план за Старк</h4>
+               <p>Според зададените от теб хранителни и двигателни навици количеството храна необходомо за отглеждането на Старк за един месец е:</p>
+            </div>
+        </div>
+    </div>
+<!-- section-1 -->
+<!-- section-2 -->
+    <div class="prodact-nutrition-wepeer">
         <div class="container">
             <div class="row">
-                <div class="lode"></div>
-                <div class="preloder-footer text-center">
-                    <h5>В момента създаваме идеалната месечна кутия на Старк.</h5>
-                    <p>Изчисляваме количеството храна необходимо за един месец. Търсим най-подходящата храна, лакомства, медикаменти и др. за един щастлив и здрав живот.</p>
+                <div class="product">
+                    <div class="product-item">
+                        <h4>Суха храна</h4>
+                        <div class="product-info">
+                            <div class="item-img text-center">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/12-kg.png' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                            <div class="item-title text-center">
+                                <h5>Advance Veterinary Diets Renal Failure </h5>
+                            </div>
+                            <div class="item-info d-flex justify-content-between">
+                                <p>Цена: 17.50 лв</p>
+                                <p>Пакет: 2 кг</p>
+                            </div>
+                            <div class="item-info-2">
+                                <p>Количество:<span> 3 пакета</span></p>
+                                <p>Абонамент<span>: - 15%</span></p>
+                                <p>Цена:<span> 52.50 лв.</span></p>
+                            </div>
+                            <div class="btns text-center">
+                                <a href="#">Промени</a>
+                            </div>
+                            <div class="deleted">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/delete.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item">
+                        <h4>Мокра храна</h4>
+                        <div class="product-info">
+                            <div class="item-img text-center">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/dog-icon.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                            <div class="item-title text-center">
+                                <h5>Няма избран продукт </h5>
+                            </div>
+                            <!-- <div class="item-info d-flex justify-content-between">
+                                <p>Цена: 17.50 лв</p>
+                                <p>Пакет: 2 кг</p>
+                            </div>
+                            <div class="item-info-2">
+                                <p>Количество:<span> 3 пакета</span></p>
+                                <p>Абонамент<span>: - 15%</span></p>
+                                <p>Цена:<span> 52.50 лв.</span></p>
+                            </div> -->
+                            <div class="btns text-center ">
+                                <a href="#">Промени</a>
+                            </div>
+                            <!-- <div class="deleted">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/delete.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div> -->
+                        </div>
+                    </div>
+                    <div class="product-item">
+                        <h4>Лакомство</h4>
+                        <div class="product-info">
+                            <div class="item-img text-center">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/12-kg.png' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                            <div class="item-title text-center">
+                                <h5>Advance Veterinary Diets Renal Failure </h5>
+                            </div>
+                            <div class="item-info d-flex justify-content-between">
+                                <p>Цена: 17.50 лв</p>
+                                <p>Пакет: 2 кг</p>
+                            </div>
+                            <div class="item-info-2">
+                                <p>Количество:<span> 3 пакета</span></p>
+                                <p>Абонамент<span>: - 15%</span></p>
+                                <p>Цена:<span> 52.50 лв.</span></p>
+                            </div>
+                            <div class="btns text-center">
+                                <a href="#">Промени</a>
+                            </div>
+                            <div class="deleted">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/delete.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item">
+                        <h4>Медикамент</h4>
+                        <div class="product-info">
+                            <div class="item-img text-center">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/12-kg.png' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                            <div class="item-title text-center">
+                                <h5>Advance Veterinary Diets Renal Failure </h5>
+                            </div>
+                            <div class="item-info d-flex justify-content-between">
+                                <p>Цена: 17.50 лв</p>
+                                <p>Пакет: 2 кг</p>
+                            </div>
+                            <div class="item-info-2">
+                                <p>Количество:<span> 3 пакета</span></p>
+                                <p>Абонамент<span>: - 15%</span></p>
+                                <p>Цена:<span> 52.50 лв.</span></p>
+                            </div>
+                            <div class="btns text-center">
+                                <a href="#">Промени</a>
+                            </div>
+                            <div class="deleted">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/delete.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item">
+                        <h4>Играчка</h4>
+                        <div class="product-info">
+                            <div class="item-img text-center">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/12-kg.png' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                            <div class="item-title text-center">
+                                <h5>Advance Veterinary Diets Renal Failure </h5>
+                            </div>
+                            <div class="item-info d-flex justify-content-between">
+                                <p>Цена: 17.50 лв</p>
+                                <p>Пакет: 2 кг</p>
+                            </div>
+                            <div class="item-info-2">
+                                <p>Количество:<span> 3 пакета</span></p>
+                                <p>Абонамент<span>: - 15%</span></p>
+                                <p>Цена:<span> 52.50 лв.</span></p>
+                            </div>
+                            <div class="btns text-center">
+                                <a href="#">Промени</a>
+                            </div>
+                            <div class="deleted">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/delete.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="product-item">
+                        <h4>Хигиена</h4>
+                        <div class="product-info">
+                            <div class="item-img text-center">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/12-kg.png' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                            <div class="item-title text-center">
+                                <h5>Advance Veterinary Diets Renal Failure </h5>
+                            </div>
+                            <div class="item-info d-flex justify-content-between">
+                                <p>Цена: 17.50 лв</p>
+                                <p>Пакет: 2 кг</p>
+                            </div>
+                            <div class="item-info-2">
+                                <p>Количество:<span> 3 пакета</span></p>
+                                <p>Абонамент<span>: - 15%</span></p>
+                                <p>Цена:<span> 52.50 лв.</span></p>
+                            </div>
+                            <div class="btns text-center">
+                                <a href="#">Промени</a>
+                            </div>
+                            <div class="deleted">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/delete.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="single-product-wepeer">
+                <div class="product-single">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-md-5 col-xxl-5">
+                            <div class="sin-product-img d-flex align-items-center">
+                                <img src="<?php echo AIRDESIGNS_DIR_IMG.'/Plan/monthly-box.svg' ?>"  alt="<?php bloginfo('name'); ?>"> 
+                                <h5>Месечна стартова кутия на Старк.</h5>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4 col-xxl-4">
+                            <div class="price-cetagory">
+                                <div class="price d-flex"><h6>Цена:</h6> <p><span class="price-sty">162.62 лв.</span></p></div>
+                                <div class="price d-flex"><h6>Спестено:</h6> <p> <span class="price">32.65 лв.</span></p></div>   
+                                <div class="price d-flex"><h6><span>Общо:</span></h6> <p><span>132.65 лв. /мес</span></p></div>   
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-3 col-xxl-3">
+                            <div class="btn-single text-center">
+                                <a href="#">Поръчай</a>
+                            </div>
+                        </div>
+                    </div>     
                 </div>
             </div>
         </div>
     </div>
-<!-- preloder -->
-<?php 
-    get_footer();
-?>
+<!-- section-2 -->
+<!-- nutrition Plan -->
